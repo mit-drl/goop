77def68a7d2b593deb00da85a078bdf58c98843e
Alex Wallar <alex@wallar.me>
2018-04-23 17:25:12 -0400

    adding library flag

--- solvers/lib.go
+++ solvers/lib.go
@@ -2,6 +2,6 @@ package solvers
 
 // #cgo CXXFLAGS: --std=c++11 -I${SRCDIR}/../.third_party/gurobi/include
 // #cgo CXXFLAGS: -I${SRCDIR}/../.third_party/lpsolve
-// #cgo LDFLAGS: -L${SRCDIR}/../.third_party/gurobi/lib -lgurobi_g++5.2 -lgurobi75
+// #cgo LDFLAGS: -L${SRCDIR}/../.third_party/gurobi/lib -lgurobi_g++5.2 -lgurobi75 -lgurobi_c++
 // #cgo LDFLAGS: -L${SRCDIR}/../.third_party/lpsolve -llpsolve55
 import "C"
