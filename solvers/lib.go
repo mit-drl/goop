760a4eb9e1a7deb38582b9abb46bc4579edae108
Kwesi Rutledge <thesolitaryecrivain@gmail.com>
2021-11-29 14:25:12 -0500

    Removing nmac-specific lib.go from tracking.

--- solvers/lib.go
+++
@@ -1,7 +0,0 @@
-package solvers
-
-// #cgo CXXFLAGS: --std=c++11 -I/Library/gurobi912/mac64/include -I/Users/kwesirutledge/Documents/Michigan/Research/goop/include
-// #cgo CXXFLAGS: -I/usr/local/opt/lp_solve/include
-// #cgo LDFLAGS: -L/usr/local/opt/lp_solve/lib -llpsolve55
-// #cgo LDFLAGS: -L/Library/gurobi912/mac64/lib -lgurobi_c++ -lgurobi91
-import "C"
